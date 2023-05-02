214b09d84a2691f6881e986fecf415ed0d0cab5b
Chandler Timm Doloriel <62779617+chandlerbing65nm@users.noreply.github.com>
2023-05-02 18:30:15 +0800

    Update distance_loss.cu

--- src/distance_loss.cu
+++ src/distance_loss.cu
@@ -26,19 +26,43 @@ __global__ void distance_loss_backward_kernel(
     const scalar_t* __restrict__ grad_output,
     const scalar_t* __restrict__ input,
     const scalar_t* __restrict__ point,
-    scalar_t* __restrict__ grad_input,
+    scalar_t* __restrict__ grad_point, // Change the name of this variable to reflect its purpose
     const size_t num_pixels) {
+  // Add two variables to store the accumulated gradients for the reference point
+  __shared__ scalar_t shared_grad_x;
+  __shared__ scalar_t shared_grad_y;
+
+  if (threadIdx.x == 0) {
+    shared_grad_x = 0;
+    shared_grad_y = 0;
+  }
+  __syncthreads();
+
   const int index = blockIdx.x * blockDim.x + threadIdx.x;
   if (index < num_pixels) {
     const scalar_t x_diff = input[index * 2] - point[0];
     const scalar_t y_diff = input[index * 2 + 1] - point[1];
-    const scalar_t distance = sqrt(x_diff * x_diff + y_diff * y_diff);
-    if (distance > 0) {
-      grad_input[index * 2] = grad_output[index] * x_diff / distance;
-      grad_input[index * 2 + 1] = grad_output[index] * y_diff / distance;
+    const scalar_t epsilon = 1e-8;
+    const scalar_t distance = sqrt(x_diff * x_diff + y_diff * y_diff) + epsilon;
+    if (distance > epsilon) {
+      // Calculate gradients with respect to the reference point's x and y coordinates
+      scalar_t grad_x = -grad_output[index] * x_diff / distance;
+      scalar_t grad_y = -grad_output[index] * y_diff / distance;
+
+      // Atomic addition to accumulate gradients across threads
+      atomicAdd(&shared_grad_x, grad_x);
+      atomicAdd(&shared_grad_y, grad_y);
     }
   }
+
+  __syncthreads();
+
+  if (threadIdx.x == 0) {
+    grad_point[0] = shared_grad_x;
+    grad_point[1] = shared_grad_y;
+  }
 }
+
 
 at::Tensor distance_loss_forward_cuda(const at::Tensor& input, const at::Tensor& point) {
   const auto num_pixels = input.size(0);
@@ -65,7 +89,7 @@ at::Tensor distance_loss_forward_cuda(const at::Tensor& input, const at::Tensor&
 
 at::Tensor distance_loss_backward_cuda(const at::Tensor& grad_output, const at::Tensor& input, const at::Tensor& point) {
   const auto num_pixels = input.size(0);
-  auto grad_input = at::zeros_like(input);
+  auto grad_point = at::zeros({2}, input.options()); // Change the tensor size to 2 (for x and y coordinates of the point)
   const int threads = 1024;
   const int blocks = (num_pixels + threads - 1) / threads;
 
@@ -74,10 +98,11 @@ at::Tensor distance_loss_backward_cuda(const at::Tensor& grad_output, const at::
         grad_output.data_ptr<scalar_t>(),
         input.data_ptr<scalar_t>(),
         point.data_ptr<scalar_t>(),
-        grad_input.data_ptr<scalar_t>(),
+        grad_point.data_ptr<scalar_t>(), // Pass the updated grad_point tensor
         num_pixels);
   }));
 
   cudaDeviceSynchronize();
-  return grad_input;
+  return grad_point;
 }
+
